1b82f42df6332e68b62d9789a1fd89b750c40b87
RobertWalker0 <algorithmicrealmist@gmail.com>
2023-08-06 19:19:15 -0700

    Adds Palindrome.py to Programming Problems
    This program checks if a word is the same spell backwards, typically for
    a palindrome. The program will exit out on the EOFError.

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+
+
+def is_palindrome(word):
+    if word == word[::-1]:
+        return True
+    return False
+
+
+def main():
+    print("Enter a word below this, we will tell you if it is a Palindrome or not.")
+
+    try:
+        word = input("Enter your word: ")
+    except EOFError as Ctrl:
+        print("Exiting the program.")
+        quit()
+    else:
+        print(is_palindrome(word))
+
+main()
+
+
